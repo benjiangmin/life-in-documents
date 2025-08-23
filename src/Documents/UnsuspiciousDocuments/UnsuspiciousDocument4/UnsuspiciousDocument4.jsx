c9683a57fe200448cadcc63fe85416faeaa2170f
Benjamin Jiang <benjaminjiang@Benjamins-MacBook-Air-2.local>
2025-08-22 20:55:48 -0500

    Added side bar to unsus4 just to check if things work

--- src/Documents/UnsuspiciousDocuments/UnsuspiciousDocument4/UnsuspiciousDocument4.jsx
+++ src/Documents/UnsuspiciousDocuments/UnsuspiciousDocument4/UnsuspiciousDocument4.jsx
@@ -1,5 +1,10 @@
+import Sidebar from "../../../../main/Sidebar"
+
 export default function Unsus4() {
     return (
-        <h1>Hello from unsus4</h1>
+        <>
+            <Sidebar />
+            <h1>Hello from unsus4</h1>
+        </>
     )
 }
