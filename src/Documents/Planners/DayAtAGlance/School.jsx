215953c4e643c7db6043b87ceec345474d7dd6c1
benjamin jiang <mercedesbenz5519@gmail.com>
2025-09-01 20:10:32 -0500

    school header

--- src/Documents/Planners/DayAtAGlance/School.jsx
+++ src/Documents/Planners/DayAtAGlance/School.jsx
@@ -1,5 +1,7 @@
 export default function School() {
     return (
-       <></>
+       <section className="school-display">
+            <h1 className="school-header">school</h1>
+       </section>
     )
 }
