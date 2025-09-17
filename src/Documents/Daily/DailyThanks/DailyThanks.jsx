50beed9229f57828f6f3abfc56499e7ece74c736
benjamin jiang <mercedesbenz5519@gmail.com>
2025-09-17 15:10:15 -0500

    layout for the daily thanks

--- src/Documents/Daily/DailyThanks/DailyThanks.jsx
+++ src/Documents/Daily/DailyThanks/DailyThanks.jsx
@@ -1,9 +1,29 @@
 import Sidebar from "../../../../main/Sidebar"
+import Title from "./Title"
+import DisplayEntries from "./displayEntries"
+import EnterEntry from "./EnterEntry"
+import Datebox from "./DateBox"
 
-export default function BiggestThanks2022() {
+export default function DailyThanks() {
     return (
-        <>
+        <section className="daily-thanks-main-display">
             <Sidebar />
-        </>
+            <section className="title-and-display-entries">
+                <section className="title-display">
+                    <Title />
+                </section>
+                 <section className="display-entries-display">
+                    <DisplayEntries />
+                </section>
+            </section>
+            <section className="datebox-and-enter-entries">
+                <section className="datebox-display">
+                    <Datebox />
+                </section>
+                <section className="enter-entry-display"> 
+                    <EnterEntry />
+                </section>
+            </section>
+        </section>
     )
 }
