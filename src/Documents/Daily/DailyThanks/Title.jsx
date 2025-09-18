532c22e7bd4327e00cea97ea60af298e33033c7d
benjamin jiang <mercedesbenz5519@gmail.com>
2025-09-17 22:32:13 -0500

    added the title block heehee so easy

--- src/Documents/Daily/DailyThanks/Title.jsx
+++ src/Documents/Daily/DailyThanks/Title.jsx
@@ -1,6 +1,8 @@
 export default function Title() {
     return (
-        <>
-        </>
+        <section className="title-inner-display">
+            <h1>the daily thanks</h1>
+            <h2>three things i'm grateful for every day</h2>
+        </section>
     )
 }
