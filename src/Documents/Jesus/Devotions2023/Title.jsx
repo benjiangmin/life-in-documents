f6c54c513603a788350e529573617db15a99c4cb
benjamin jiang <mercedesbenz5519@gmail.com>
2025-10-09 18:40:46 -0500

    whoops my css was named the same thing as something else

--- src/Documents/Jesus/Devotions2023/Title.jsx
+++ src/Documents/Jesus/Devotions2023/Title.jsx
@@ -1,6 +1,6 @@
 export default function Title() {
     return (
-        <section className="title-inner-display">
+        <section className="title-inner-display for-devotions">
             <h1>devotions</h1>
             <h2>2023</h2>
         </section>
